babafc05aed01e7f5242445a662000e93dcf9d32
k-nautresurrect <kushignite@gmail.com>
2022-02-26 11:31:27 +0530

    Added percentage in piechart

--- portal/feb22/src/components/PieChart/PieChart.js
+++ portal/feb22/src/components/PieChart/PieChart.js
@@ -3,23 +3,42 @@ import React from "react";
 // importing components used for building chart
 import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 import "./Piechart.css";
 
 // registered component as per v4
-Chart.register(ArcElement, Tooltip, Legend);
+Chart.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 // set options for chart
+
 const options = {
   responsive: true,
+  plugins: {
+    datalabels: {
+      formatter: (value, context) => {
+        const percent = ((value / sum) * 100).toFixed(1);
+        console.log(context);
+        return `${percent}%`;
+      },
+      color: "rgb(0,0,0)",
+    },
+  },
 };
 
 // set labels, which represent the partitions in chart and values
 const labels = ["undergraduate", "graduate", "professionals"];
 const values = [123, 54, 78];
 
+// to get sum from the array.
+const sum = values.reduce((Sum, a) => Sum + a, 0);
+
 // color schemes for partition
-const colArr = ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 206, 86)"];
+const colArr = [
+  "rgba(13, 166, 255, 0.6)",
+  "rgba(187, 30, 30, 0.6)",
+  "rgba(3, 219, 93, 0.6)",
+];
 
 // set dataset, data is set according to the labels
 const dataset = [
